0ec569240d3f0cbd2a203a2f89587428e73c2681
AnaBarrena <ana.barrenam@yahoo.es>
2019-05-28 18:37:32 +0200

    Java Scrpit

--- 09_intro_js/05_constantes.js
+++ 09_intro_js/05_constantes.js
@@ -9,28 +9,40 @@ console.log(a)
 
 // ES6
 const b = 6
+// Error b = 23
+
 
 if (true) {
     const c = 7
+    console.log(b) // 6
+    console.log(c) // 7
 }
 
 console.log(b) // 6
 // console.log(c) // Error
 
-/////////////////
+////////////////
 
-let datos = [22, 10, 36]
-let persona = {nombre: 'Pepe', edad: 22}
+const datos = [22, 10, 36]
+const persona = {nombre: 'Pepe', edad: 22}
 
 datos[datos.length] = 63
 datos.push(43)
 
-// datos = 3 Error
+// datos = 3 // Error
+// datos = []
+// datos = [22, 10, 36, 63, 43]
+
 
 persona.altura = 178
 persona.edad = 23
+persona.nombre = ''
+delete persona.nombre
+persona.nombre = ''
 
 // persona = 178
+// persona = {}
+// persona = {nombre: 'Pepe', edad: 22}
 
 console.log(datos)
-console.log(persona, nombre)
+console.log(persona)
