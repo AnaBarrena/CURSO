53cd60217cf5a891afdb21bc6901b2e493a573f3
anabarrena <ana.barrenam@yahoo.es>
2019-06-27 20:41:06 +0200

    Java Script

--- JS/09_async.js
+++ JS/09_async.js
@@ -1,7 +1,16 @@
-/** Asincronía */
+/** Asincronía
+ * 
+ * En otros lenguajes es multhilo - multithread
+ * Dándose concurrencia real
+ * 
+ * En JavaScript hay un solo hilo
+ * que gestiona el bucle de eventos
+ */
 
+
+
+ 
 setTimeout( () => {
     console.log('Pasaron dos segundos')
 }, 2000)
-
 console.log('Fin')
