a0ba0f89f07bf44bac07c7d5ab93c03798c402ba
anabarrena <ana.barrenam@yahoo.es>
2019-06-27 20:36:49 +0200

    Java Script

--- JS/08_callbacks.js
+++ JS/08_callbacks.js
@@ -1,9 +1,10 @@
 function calcular(a, b, operacion) {
-    let msg = `El resultado de una ${operacion} con ${a} y ${b} es`
+    let msg = `El resultado de un/a ${operacion} con ${a} y ${b} es`
     return msg + callback(a,b)
 }
 
-console.log(calcular(4,6,'pepe', function(x,y) {return x + y}))
-console.log(calcular(4,6,'juan', function(x,y) {return x + y}))
-console.log(calcular(4,6,'producto', function(x,y) {return x + y}))
+function producto (a,b) {return a*b}
 
+console.log(calcular(4,6,'pepe', function(x,y) {return x + y}))
+console.log(calcular(4,6,'juan', function(x,y) {return x - y}))
+console.log(calcular(4,6,'producto', function(x,y) {return x + y}))
---
+++ JS/09_async.js
@@ -0,0 +1,7 @@
+/** Asincronía */
+
+setTimeout( () => {
+    console.log('Pasaron dos segundos')
+}, 2000)
+
+console.log('Fin')
