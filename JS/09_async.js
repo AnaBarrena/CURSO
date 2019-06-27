81368f18efb0e8758e256c41fea5035eba6ffb13
anabarrena <ana.barrenam@yahoo.es>
2019-06-27 21:16:20 +0200

    Java Script

--- JS/09_async.js
+++ JS/09_async.js
@@ -7,10 +7,13 @@
  * que gestiona el bucle de eventos
  */
 
-
-
  
 setTimeout( () => {
-    console.log('Pasaron dos segundos')
+    console.log('Mensaje 1')
 }, 2000)
-console.log('Fin')
+console.log('Mensaje 2')
+console.log("Mensaje 3")
+setTimeout( () => {
+    console.log('Mensaje 4')
+}, 2000)
+console.log('Mensaje 5')
